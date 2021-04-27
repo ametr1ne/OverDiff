f1794d806a1720e32bb4f429b0e9a94aefca8d2f
UndFrame <undframe@gmail.com>
2021-04-27 09:41:46 +0400

    add auth activity

--- app/src/main/java/com/ametr1ne/overdiff/MainActivity.java
+++ app/src/main/java/com/ametr1ne/overdiff/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_authorization);
+
     }
 }
