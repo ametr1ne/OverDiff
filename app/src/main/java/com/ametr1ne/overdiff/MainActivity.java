2e6228e9fa6b51ce923a1c33f07dadcda7645d35
UndFrame <undframe@gmail.com>
2021-05-05 14:18:36 +0400

    bug fix

--- app/src/main/java/com/ametr1ne/overdiff/MainActivity.java
+++ app/src/main/java/com/ametr1ne/overdiff/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ametr1ne.overdiff;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.ametr1ne.overdiff.fragments.AuthFragment;
@@ -26,11 +31,41 @@ public class MainActivity extends AppCompatActivity {
 
     private static FileProperties properties;
 
+    private static final int PERMISSION_CALL = 127;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED) {
+            makeCall();
+        }else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_CALL);
+        }
+
+
+
+        System.out.println("REFRESH: "+UserFactory.getInstance().getCurrentUser().getRefreshToken());
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CALL) {
+
+            boolean access = true;
+
+            for (int grantResult : grantResults) {
+                access &= grantResult == PackageManager.PERMISSION_GRANTED;
+            }
+
+            if(access)
+                makeCall();
+        }
+    }
+
+    public void makeCall(){
         RawProperties rawProperties = new RawProperties(getResources().openRawResource(R.raw.application));// getting XML
         rawProperties.getValue("ksite").ifPresent(s -> {
             GlobalProperties.KSITE_ADDRESS = s;
@@ -54,11 +89,7 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new TapesFragment(this)).commit();
         });
-
-        System.out.println("REFRESH: "+UserFactory.getInstance().getCurrentUser().getRefreshToken());
-
     }
-
 
     public static FileProperties getProperties(){
         return properties;
